182243e4817acd545ac572fea5bf143a5d2209b0
Ignatius Ngwaabanjong <ignatiusngwaabanjong@Ignatiuss-MBP.attlocal.net>
2023-08-26 14:14:57 -0500

    Updated description

--- deploy-vpc-ec2-with-terraform/7-ec2instance.tf
+++ deploy-vpc-ec2-with-terraform/7-ec2instance.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "MyServer" {
   ami                         = "ami-0ff8a91507f77f867"
   instance_type               = "t2.micro"
-  key_name                    = "bjuatkey.pem"
+  key_name                    = "bjuatkey"
   monitoring                  = true
   subnet_id                   = aws_subnet.public2.id
   vpc_security_group_ids      = [aws_security_group.MySg.id]
   associate_public_ip_address = true
 
   //CODE//
-# for_each = toset(["master", "slave"])
+# for_each = toset(["node-1", "node-2"])
 #    tags = {
 #      Name = "${each.key}-${var.project-name}-ec2"
 #    }
--- deploy-vpc-ec2-with-terraform/variables.tf
+++ deploy-vpc-ec2-with-terraform/variables.tf
@@ -1,1 +1,3 @@
-variable "project-name" {}
+variable "project-name" {
+    Description = "Write the project name in the .tfvars file"
+}
